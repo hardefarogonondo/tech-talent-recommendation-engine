603396f664a4fb2c54768cfe829507afe16095d8
Hardefa Rogonondo <hardefarogonondo@gmail.com>
2023-11-04 17:55:04 +0700

    Candidates recommendation function updated

--- src/back_end/main.py
+++ src/back_end/main.py
@@ -2,6 +2,7 @@
 from config.config import BINARY_RELEVANCE_ENCODED_VALUES, COLUMN_NEW_ORDER, COLUMN_RENAME_DICT, ONE_HOT_ENCODED_VALUES, Requirements
 from fastapi import FastAPI
 from ml_from_scratch.MatrixFactorization import AlternatingLeastSquaresFactorization
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
 import json
 import numpy as np
@@ -9,11 +10,12 @@ import pandas as pd
 import pickle
 
 # Initialization
+df_original = pd.read_pickle('../../data/processed/df_preprocessed.pkl')
+df_encoded = pd.read_pickle('../../data/processed/df_processed_encoded.pkl')
 with open('../../models/alsf_model.pkl', 'rb') as file:
     model = pickle.load(file)
 with open('../../models/scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
-# train_df = pd.read_pickle('/app/data/processed/train.pkl')
 app = FastAPI()
 
 
@@ -24,7 +26,7 @@ def rename_and_sort_columns(df):
 
 
 def feature_scaler(df):
-    df["YearsCode"] = scaler.transform(df["YearsCode"].values.reshape(-1, 1))
+    df[["YearsCode"]] = scaler.transform(df[["YearsCode"]])
     return df
 
 
@@ -56,6 +58,19 @@ def one_hot_encoding(df, columns_possibilities=ONE_HOT_ENCODED_VALUES):
     return df
 
 
+def predict(df):
+    prediction = model.predict(df)
+    return prediction
+
+
+def find_most_similar_candidates(predicted_values, df_encoded=df_encoded, df_original=df_original, top_n=10):
+    similarities = cosine_similarity([predicted_values], df_encoded.values)
+    sorted_indices = np.argsort(similarities[0])[::-1]
+    top_candidates_indices = sorted_indices[:top_n]
+    top_candidates_with_identifiers = df_original.iloc[top_candidates_indices]
+    return top_candidates_with_identifiers
+
+
 @app.post('/recommend')
 def recommend_candidate(requirements: Requirements):
     df = pd.DataFrame([requirements.dict()])
@@ -63,7 +78,7 @@ def recommend_candidate(requirements: Requirements):
     df = feature_scaler(df)
     df = binary_relevance_encoding(df)
     df = one_hot_encoding(df)
-    print(df.columns)
-    print(df[["OfficeStackSyncHaveWorkedWith_Zoom",
-          "OfficeStackSyncHaveWorkedWith_Zulip"]])
-    return {"message": "DataFrame printed to terminal", "data": requirements}
+    prediction = predict(df).flatten()
+    similar_candidates = find_most_similar_candidates(prediction)
+    candidates = similar_candidates.to_dict(orient='records')
+    return {"recommended_candidates": candidates}
